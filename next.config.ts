24541697d2a832a4f84f81bece90a034a037e170
mudalideco <mudali@protonmail.com>
2026-06-25 10:14:07 +0300

    Add Next.js config

---
+++ next.config.ts
@@ -0,0 +1,16 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  turpopack: {
+    root: process.cwd(),
+  },
+};
+
+export default nextConfig;
